30886077093ceea761b4fde181fd73147f41e576
orlando <orlandoesteban@hotmail.cl>
2018-04-19 11:53:17 -0300

    variable de entorno mongo heroku

--- server/config/config.js
+++ server/config/config.js
@@ -10,10 +10,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
+//EL MONGO_URI lo cree como variable de entonrno con heroku
+//para crearlo es con 
+//heroku config:set MONGO_URI="URL"
+//para buscarlo HEROKU CONFIG:GET MONGO_URI
+//para borrarlo HEROKU CONFIG:UNSET MONGO_URI
+//DESPUES DE HABERLA CREADO, SE PUEDE USAR, ESTO ES PARA QUE NO QUEDE EXPUESTA EN GITHUB, Y LA RECONOSCA HEROKU
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123456@ds251799.mlab.com:51799/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
